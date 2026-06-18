0332f660c43a4af42b5d275ffcff6b97205ccb63
Sakshi Chobhe <chobhesakshi589@gmail.com>
2026-06-18 14:49:56 +0530

    Add files via upload

---
+++ Computer_vision.py
@@ -0,0 +1,70 @@
+﻿import os
+import cv2
+import mediapipe as mp
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python import BaseOptions
+
+#read the image
+image_path=os.path.join('.','images','face.png')
+image=cv2.imread(image_path)
+
+H,W,C=image.shape
+
+
+
+#1-detect the path model
+model_path=os.path.join('.','blazeface_common.task')
+
+options=BaseOptions(model_asset_path=model_path)
+
+#2-put the model in the face detector
+base_options=vision.FaceDetectorOptions(base_options=options,min_detection_confidence = 0.5)
+#3-create the face detector object, the word with is important to clear the face detector when we not wanted that
+with vision.FaceDetector.create_from_options(base_options) as face_detector:
+    #4-cover the image not change to rgb cus the mediapipe is clever
+
+    ready_img=mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+                 #5-show the result
+    final_result=face_detector.detect(ready_img)
+
+#6 check
+
+if final_result.detections:
+    for face in final_result.detections:
+
+        #7- use bounding_box to return origin_x,origin_y,height,width
+        box=face.bounding_box
+
+        #8- set padding to increase accuracy
+        padding=30
+        old_x= box.origin_x
+        old_y= box.origin_y
+        old_h= box.height
+        old_w= box.width
+
+        x1=max(0,old_x-padding)
+        y1=max(0,old_y-padding)
+       
+        x2=min(W,(old_x+old_w)+padding)
+        y2=min(H,(old_y+old_h)+padding)
+
+
+        
+
+        #10- face anonymizer
+        face_roi=image[y1:y2,x1:x2]
+        blurred_face=cv2.GaussianBlur(face_roi,(99,99),0)
+        image[y1:y2,x1:x2]  =  blurred_face
+        cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,0),2)
+
+
+cv2.imshow(' Face Anonymizer',image)
+cv2.waitKey(0)  
+
+
+
+
+    
+
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,25 @@
+﻿# Face Anonymizer 👤✨
+
+A simple Python project that detects faces in images and blurs them automatically to protect privacy. It uses **OpenCV** and Google's **MediaPipe Tasks API**.
+
+The project automatically adds extra space (padding) around the face to make sure the whole head, including hair and chin, is completely hidden.
+
+---
+
+## 🚀 Features
+* **Fast Face Detection:** Uses MediaPipe's BlazeFace model for quick detection.
+* **Full Head Blur:** Adds smart padding so the entire face area is covered.
+* **Safe Boundaries:** Uses `min` and `max` functions to prevent code crashes if the face is near the edge of the image.
+
+---
+
+## 🛠️ Requirements
+Before running the code, you need to install these Python libraries:
+
+```bash
+pip install opencv-python mediapip
+
+Note: Make sure to download the blazeface_common.task model file and place it in the main project folder
+
+Bash
+python Computer_vision.py
